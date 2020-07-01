324e6993d1a7425f35e2df450db327bf43672b24
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2020-07-01 22:56:46 +0530

    added code to parse the json response

---
+++ youtube-onedaybuild/Model.swift
@@ -0,0 +1,56 @@
+//
+//  Model.swift
+//  youtube-onedaybuild
+//
+//  Created by Amit Kumar on 30/06/20.
+//  Copyright © 2020 Amit Kumar. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    func getVideos() {
+        
+        // Create a URL object
+        let url = URL(string: Constants.API_URL)
+        
+        guard url != nil else{
+            return
+        }
+        
+        // Get a URLSession object
+        let session = URLSession.shared
+        
+        // Get a data task from the URLSession object
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            // Check if there were any errors
+            if error != nil || data == nil {
+                return
+            }
+            
+            do {
+                // Parsing the data into video objects
+                           
+                let decoder = JSONDecoder()
+                           
+                decoder.dateDecodingStrategy = .iso8601
+                           
+                let response = try decoder.decode(Response.self, from: data!)
+                
+                dump(response)
+                
+            }
+            catch{
+                
+            }
+           
+        
+        }
+        
+        // kick off the task
+        dataTask.resume()
+        
+    }
+}
---
+++ youtube-onedaybuild/Response.swift
@@ -0,0 +1,28 @@
+//
+//  Response.swift
+//  youtube-onedaybuild
+//
+//  Created by Amit Kumar on 01/07/20.
+//  Copyright © 2020 Amit Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init (from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+        
+    }
+}

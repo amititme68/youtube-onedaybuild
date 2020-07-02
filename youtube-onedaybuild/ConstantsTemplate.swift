585572119d3c6129c75c5f43dd8d5c46f9dfc117
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2020-07-02 23:07:58 +0530

    modified gitignore to stop tracking Constants.swift,added ConstantsTemplate.swift instead

--- youtube-onedaybuild/ConstantsTemplate.swift
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -1,16 +1,19 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youtube-onedaybuild
 //
-//  Created by Amit Kumar on 30/06/20.
+//  Created by Amit Kumar on 02/07/20.
 //  Copyright © 2020 Amit Kumar. All rights reserved.
 //
 
 import Foundation
 
-struct Constants {
+/*
+ struct Constants {
     static var API_KEY = ""
     static var PLAYLIST_ID = "UUCjiKZ6ZbYVlxDfat8pcohQ"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell"
     
 }
+ */
